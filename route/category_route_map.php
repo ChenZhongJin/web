046f081f10f2f38a09a5c79be5d2c5d90d230058
ChenZhongJin <870088923@qq.com>
2018-07-17 16:48:14 +0800

    测试调整

--- route/category_route_map.php
+++
@@ -1,6 +0,0 @@
-<?php
-Route::get('event','home/Home/senseRoute')->name('event');
-Route::get('products','home/Home/senseRoute')->name('products');
-Route::get('modifiedsinewave','home/Home/senseRoute')->name('modifiedsinewave');
-Route::get('puresinewavesp','home/Home/senseRoute')->name('puresinewavesp');
-Route::get('support','home/Home/senseRoute')->name('support');
