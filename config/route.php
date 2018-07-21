757da7521d49c9b034dd9fe9aa270f6bb8b5206e
chenzhongjin <870088923@qq.com>
2018-07-21 12:49:10 +0800

    上传包

--- config/route.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-
-    'route_annotation' =>true
-];
--- tasks/collectTasks.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Crunz\Schedule;
-$schedule = new Schedule();
-$path = __DIR__ .'/../';
-$schedule->run("php $path/think task:collect")       
-        ->everyMinute()
-        ->description('Create a backup of the project directory.');
-return $schedule;
